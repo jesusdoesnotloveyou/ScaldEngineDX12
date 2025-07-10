9deace6b7085a33845a5bfeab34a8236fa9ecdc6
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-07-10 20:55:25 +0800

    const buffer struct

--- Engine/Src/Core/ScaldCoreTypes.h
+++ Engine/Src/Core/ScaldCoreTypes.h
@@ -9,3 +9,12 @@ struct Vertex
 	XMFLOAT3 position;
 	XMFLOAT4 color;
 };
+
+// Constant buffers
+
+struct SceneConstantBuffer
+{
+	XMFLOAT4 offset = XMFLOAT4(0.0f, 0.0f, 0.0f, 0.0f);
+	float pad[60];
+};
+static_assert((sizeof(SceneConstantBuffer) % 256) == 0, "Constant Buffer size must be 256-byte aligned");
