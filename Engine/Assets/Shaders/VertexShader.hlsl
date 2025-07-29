ed8fdcda3540231e8df94169bfd91cbcc0ec317c
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2025-07-29 23:56:04 +0800

    add constant buffer

--- Engine/Assets/Shaders/VertexShader.hlsl
+++ Engine/Assets/Shaders/VertexShader.hlsl
@@ -1,23 +1,28 @@
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorldViewProj;
+}
+
 struct VSInput
 {
-    float4 inPosition : POSITION0;
-    float4 inColor : COLOR0;
+    float4 iPosL : POSITION0;
+    float4 iColor : COLOR0;
     //float2 inTexCoord : TEXCOORD0;
     //float3 inNormal : NORMAL;
 };
 
 struct VSOutput
 {
-    float4 outPosition : SV_POSITION;
-    float4 outColor : COLOR0;
+    float4 oPosH : SV_POSITION;
+    float4 oColor : COLOR0;
 };
 
 VSOutput main(VSInput input)
 {
     VSOutput output;
     
-    output.outPosition = input.inPosition;
-    output.outColor = input.inColor;
+    output.oPosH = input.iPosL;
+    output.oColor = input.iColor;
 	
     return output;
 }
