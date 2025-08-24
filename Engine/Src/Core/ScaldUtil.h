cbe69f80246c258588232f053c57156f0adc7fc5
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-08-24 23:38:15 +0800

    a convenient structure for representing meshes

--- Engine/Src/Core/ScaldUtil.h
+++ Engine/Src/Core/ScaldUtil.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "DXHelper.h"
+#include <unordered_map>
+#include "DirectXCollision.h"
 
 using Microsoft::WRL::ComPtr;
 
@@ -10,4 +12,76 @@ public:
     static ComPtr<ID3D12Resource> CreateDefaultBuffer(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, const void* initData, UINT64 byteSize, ComPtr<ID3D12Resource>& uploadBuffer);
 
     static UINT CalcConstantBufferByteSize(const UINT byteSize);
+};
+
+// Defines a subrange of geometry in a MeshGeometry.  This is for when multiple
+// geometries are stored in one vertex and index buffer.  It provides the offsets
+// and data needed to draw a subset of geometry stores in the vertex and index 
+// buffers so that we can implement the technique described by Figure 6.3.
+struct SubmeshGeometry
+{
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	INT BaseVertexLocation = 0;
+
+	// Bounding box of the geometry defined by this submesh. 
+	// This is used in later chapters of the book.
+	DirectX::BoundingBox Bounds;
+};
+
+struct MeshGeometry
+{
+	// Give it a name so we can look it up by name.
+	std::string Name;
+
+	// System memory copies.  Use Blobs because the vertex/index format can be generic.
+	// It is up to the client to cast appropriately.  
+	Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
+
+	// the actual default buffer resource
+	Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
+
+	// an intermediate upload heap in order to copy CPU memory data into out default buffer
+	Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
+
+	// Data about the buffers.
+	UINT VertexByteStride = 0u;
+	UINT VertexBufferByteSize = 0u;
+	DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;
+	UINT IndexBufferByteSize = 0u;
+
+	// A MeshGeometry may store multiple geometries in one vertex/index buffer.
+	// Use this container to define the Submesh geometries so we can draw
+	// the Submeshes individually.
+	std::unordered_map<std::string, SubmeshGeometry> DrawArgs;
+
+	D3D12_VERTEX_BUFFER_VIEW VertexBufferView()const
+	{
+		D3D12_VERTEX_BUFFER_VIEW vbv;
+		vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+		vbv.StrideInBytes = VertexByteStride;
+		vbv.SizeInBytes = VertexBufferByteSize;
+
+		return vbv;
+	}
+
+	D3D12_INDEX_BUFFER_VIEW IndexBufferView()const
+	{
+		D3D12_INDEX_BUFFER_VIEW ibv;
+		ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+		ibv.Format = IndexFormat;
+		ibv.SizeInBytes = IndexBufferByteSize;
+
+		return ibv;
+	}
+
+	// We can free this memory after we finish upload to the GPU.
+	void DisposeUploaders()
+	{
+		VertexBufferUploader = nullptr;
+		IndexBufferUploader = nullptr;
+	}
 };
