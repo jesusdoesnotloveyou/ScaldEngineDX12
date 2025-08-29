499f33d70fb77d75940d7fd17fb48040aa255fb9
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-08-29 14:39:09 +0300

    add advanced vertex structure; add pass constant buffer structure; asserts have been removed since ScaldUtil::CalcConstantBufferByteSize

--- Engine/Src/Core/ScaldCoreTypes.h
+++ Engine/Src/Core/ScaldCoreTypes.h
@@ -11,11 +11,52 @@ struct Vertex
 	XMFLOAT4 color;
 };
 
-// Constant buffers
+struct SVertex
+{
+	XMFLOAT3 position;
+	XMFLOAT3 normal;
+	XMFLOAT3 tangent;
+	XMFLOAT2 texCoord;
 
+	SVertex(const XMFLOAT3& p, const XMFLOAT3& n, const XMFLOAT3& t, const XMFLOAT2& uv)
+		:
+		position(p),
+		normal(n),
+		tangent(t),
+		texCoord(uv)
+	{}
+	
+	SVertex(
+		float px, float py, float pz,
+		float nx, float ny, float nz,
+		float tx, float ty, float tz,
+		float u, float v)
+		:
+		position(px, py, pz),
+		normal(nx, ny, nz),
+		tangent(tx, ty, tz),
+		texCoord(u, v)
+	{}
+};
+
+// Constant buffers
 struct ObjectConstants
 {
-	XMFLOAT4X4 gWorldViewProj;
-	float pad[48];
+	XMFLOAT4X4 World;
 };
-static_assert((sizeof(ObjectConstants) % 256) == 0, "Constant Buffer size must be 256-byte aligned");
+
+struct PassConstants
+{
+	XMFLOAT4X4 View;
+	XMFLOAT4X4 Proj;
+	XMFLOAT4X4 ViewProj;
+	XMFLOAT4X4 invViewProj;
+
+	XMFLOAT3 EyePosW = { 0.0f, 0.0f, 0.0f };
+	float pad1 = 0.0f;
+	
+	float NearZ;
+	float FarZ;
+	float DeltaTime;
+	float TotalTime;
+};
