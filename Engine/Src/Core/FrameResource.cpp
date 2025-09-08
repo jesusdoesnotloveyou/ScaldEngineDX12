dff6531260955c9f8bcd04fb9be2086eca7093aa
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-09-08 13:19:48 +0300

    new constant buffer for materials in frame resource has been added

--- Engine/Src/Core/FrameResource.cpp
+++ Engine/Src/Core/FrameResource.cpp
@@ -1,12 +1,13 @@
 #include "stdafx.h"
 #include "FrameResource.h"
 
-FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount)
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount)
 {
 	ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(commandAllocator.GetAddressOf())));
 
 	ObjectsCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, TRUE);
 	PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, TRUE);
+	MaterialCB = std::make_unique<UploadBuffer<MaterialConstants>>(device, materialCount, TRUE);
 }
 
 FrameResource::~FrameResource()
