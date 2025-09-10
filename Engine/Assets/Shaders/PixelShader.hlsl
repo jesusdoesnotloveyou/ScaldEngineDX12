9347c95036d92bfdb4bb8a028d8fbc4c0820f19c
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-09-10 17:07:03 +0300

    [IMPORTANT] blinn-phong lighting model implemented (for dir and point lights); shader comments for details have been added

--- Engine/Assets/Shaders/PixelShader.hlsl
+++ Engine/Assets/Shaders/PixelShader.hlsl
@@ -23,6 +23,13 @@ struct Light
     float SpotPower;
 };
 
+struct Material
+{
+    float4 DiffuseAlbedo;
+    float3 FresnelR0;
+    float Shininess; // 1 - roughness
+};
+
 cbuffer cbPerMaterial : register(b1)
 {
     float4 gDiffuseAlbedo;
@@ -55,12 +62,38 @@ struct PSInput
     float3 iNormalW : NORMAL;
 };
 
-float3 CalcDirLight(Light L, float3 N)
+float3 SchlickApproximation(float3 fresnelR0, float3 halfVec, float3 lightDir)
+{
+    float cosIncidentAngle = saturate(dot(halfVec, lightDir)); // clamp between [0, 1] -> [0, 90] degrees
+    float f0 = 1.0f - cosIncidentAngle;
+    float3 reflection = fresnelR0 + (1.0f - fresnelR0) * (f0 * f0 * f0 * f0 * f0);
+    return reflection;
+}
+
+// Check F.Luna Introduction to DirectX12 paragraph 8.8 to see lighting model details.
+float3 BlinnPhong(float3 lightStrength, float3 lightDir, float3 N, float3 viewDir, Material mat)
+{
+    const float m = mat.Shininess * 256.0f;
+    
+    float3 halfVec = normalize(viewDir + lightDir);
+    
+    float3 schlick = SchlickApproximation(mat.FresnelR0, halfVec, lightDir);
+    float normFactor = (m + 8.0f) / 8.0f;
+    float nh = pow(max(dot(halfVec, N), 0.0f), m);
+    
+    float3 specAlbedo = schlick * normFactor * nh;
+    
+    specAlbedo = specAlbedo / (specAlbedo + 1.0f);
+    
+    return (mat.DiffuseAlbedo.rgb + specAlbedo) * lightStrength;
+}
+
+float3 CalcDirLight(Light L, float3 N, float3 viewDir, Material mat)
 {
     float3 lightDirection = normalize(-L.Direction);
-    
     float NdotL = max(dot(lightDirection, N), 0.0f);
-    return NdotL * L.Strength;
+    float3 lightStrength = NdotL * L.Strength;
+    return BlinnPhong(lightStrength, lightDirection, N, viewDir, mat);
 }
 
 float CalcAttenuation(float distance, float falloffStart, float falloffEnd)
@@ -68,7 +101,7 @@ float CalcAttenuation(float distance, float falloffStart, float falloffEnd)
     return saturate((falloffEnd - distance) / (falloffEnd - falloffStart));
 }
 
-float3 CalcPointLight(Light L, float3 N, float3 posW, float3 toEye)
+float3 CalcPointLight(Light L, float3 N, float3 posW, float3 viewDir, Material mat)
 {
     float3 lightVec = L.Position - posW;
     
@@ -80,14 +113,64 @@ float3 CalcPointLight(Light L, float3 N, float3 posW, float3 toEye)
     float3 lightDir = lightVec / d;
     float NdotL = max(dot(lightDir, N), 0.0f);
     float attenuation = CalcAttenuation(d, L.FallOfStart, L.FallOfEnd);
+    float3 lightStrength = L.Strength * NdotL * attenuation;
     
-    return L.Strength * NdotL * attenuation;
+    return BlinnPhong(lightStrength, lightDir, N, viewDir, mat);
 }
 
-float3 CalcSpotLight(Light L, float3 N, float3 posW, float3 toEye)
+float3 CalcSpotLight(Light L, float3 N, float3 posW, float3 viewDir, Material mat)
 {
     
     return float3(1.f, 1.f, 1.f);
+}
+
+// The idea is to calculate every light object's illumination strength related to it's type specific parameters
+// and to pass it in BlinnPhong model function
+float4 ComputeLight(float3 N, float3 posW)
+{
+    Material mat = { gDiffuseAlbedo, gFresnelR0, 1.0f - Roughness };
+    float3 viewDir = normalize(gEyePos - posW);
+    
+    float3 litColor = 0.0f;
+    
+#if NUM_DIR_LIGHTS
+    
+    float3 dirLight = 0.f;
+    
+    [unroll]
+    for (int i = 0; i < NUM_DIR_LIGHTS; i++)
+    {
+        dirLight += CalcDirLight(gLights[i], N, viewDir, mat);
+    }
+    litColor += dirLight;
+#endif
+    
+#if NUM_POINT_LIGHTS
+    
+    float3 pointLight = 0.f;
+    
+    [unroll]
+    for (i = NUM_DIR_LIGHTS; i < NUM_DIR_LIGHTS + NUM_POINT_LIGHTS; i++)
+    {
+        pointLight += CalcPointLight(gLights[i], N, posW, viewDir, mat);
+
+    }
+    litColor += pointLight;
+#endif
+    
+#if NUM_SPOT_LIGHTS
+    
+    float3 spotLight = 0.f;
+    
+    [unroll]
+    for (i = NUM_DIR_LIGHTS + NUM_POINT_LIGHTS; i < NUM_DIR_LIGHTS + NUM_POINT_LIGHTS + NUM_SPOT_LIGHTS; i++)
+    {
+        spotLight += CalcSpotLight(gLights[i], N, posW, viewDir, mat);
+    }
+    litColor += spotLight;
+#endif
+    
+    return float4(litColor, 0.0f);
 }
 
 float4 main(PSInput input) : SV_TARGET
@@ -97,40 +180,6 @@ float4 main(PSInput input) : SV_TARGET
     float4 ambient = gAmbient * gDiffuseAlbedo;
     float4 litColor = ambient;
     
-#if NUM_DIR_LIGHTS
-    
-    float3 dirLight = float3(0.f, 0.f, 0.f);
-    
-    [unroll]
-    for (int i = 0; i < NUM_DIR_LIGHTS; i++)
-    {
-        dirLight += CalcDirLight(gLights[i], N);
-    }
-    litColor += float4(dirLight, 0.0f);
-#endif
-    
-#if NUM_POINT_LIGHTS
-    
-    float3 pointLight = float3(0.f, 0.f, 0.f);
-    
-    [unroll]
-    for (i = NUM_DIR_LIGHTS; i < NUM_DIR_LIGHTS + NUM_POINT_LIGHTS; i++)
-    {
-        pointLight += CalcPointLight(gLights[i], N, input.iPosW, gEyePos);
-    }
-    litColor += float4(pointLight, 0.0f);
-#endif
-    
-#if NUM_SPOT_LIGHTS
-    
-    float3 spotLight = float3(0.f, 0.f, 0.f);
-    
-    [unroll]
-    for (i = NUM_DIR_LIGHTS + NUM_POINT_LIGHTS; i < NUM_DIR_LIGHTS + NUM_POINT_LIGHTS + NUM_SPOT_LIGHTS; i++)
-    {
-        spotLight += CalcSpotLight(gLights[i], N, input.iPosW, gEyePos);
-    }
-    litColor += float4(spotLight, 0.0f);
-#endif
+    litColor += ComputeLight(N, input.iPosW);
     return litColor;
 }
