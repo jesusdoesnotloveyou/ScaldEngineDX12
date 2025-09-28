10246d59810dad196b7b7859ee7315ee8fb25052
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-09-28 14:29:32 +0300

    add shader for common resources between pipeline stages

---
+++ Engine/Assets/Shaders/Common.hlsl
@@ -0,0 +1,47 @@
+#include "LightUtil.hlsl"
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+    float4x4 gTexTransform;
+};
+
+cbuffer cbPerMaterial : register(b1)
+{
+    float4 gDiffuseAlbedo;
+    float3 gFresnelR0;
+    float gRoughness;
+    float4x4 gMatTransform;
+};
+
+cbuffer cbPerPass : register(b2)
+{
+    float4x4 gView;
+    float4x4 gProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePos;
+    float pad1;
+    float gNearZ;
+    float gFarZ;
+    float gDeltaTime;
+    float gTotalTime;
+    
+    float4 gAmbient;
+    
+    float4 gFogColor;
+    float gFogStart;
+    float gFogRange;
+    float2 pad2;
+    
+    Light gLights[MaxLights];
+};
+
+Texture2D gDiffuseMap : register(t0);
+Texture2D gShadowMap : register(t1);
+
+SamplerState gSamplerPointWrap : register(s0);
+SamplerState gSamplerLinearWrap : register(s1);
+SamplerState gSamplerAnisotropicWrap : register(s2);
+SamplerState gShadowSamplerLinearBorder : register(s3);
+SamplerComparisonState gShadowSamplerComparisonLinearBorder : register(s4);
