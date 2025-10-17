2b7c3ecee9a661dc4e4b34854b1b943bbcab3971
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-10-17 12:41:01 +0300

    GBuffer setup: create resources and descriptors

--- Engine/Src/Core/GBuffer.cpp
+++ Engine/Src/Core/GBuffer.cpp
@@ -1,18 +1,115 @@
 #include "stdafx.h"
 #include "GBuffer.h"
 
-GBuffer::GBuffer(ID3D12Device* device)
-	: m_device(device)
-	
+GBuffer::GBuffer(ID3D12Device* device, UINT width, UINT height)
+    : m_device(device)
+    , m_width(width)
+    , m_height(height)
 {
+    m_buffer[EGBufferLayer::DEPTH].m_isDSV = true;
 
+    CreateResources();
 }
 
 GBuffer::~GBuffer() noexcept
 {
 }
 
+void GBuffer::OnResize(UINT newWidth, UINT newHeight)
+{
+    if (m_width != newWidth || m_height != newHeight)
+    {
+        m_width = newWidth;
+        m_height = newHeight;
+        
+        CreateResources();
+
+        // New resource, so we need new descriptors to that resource.
+        CreateDescriptors();
+    }
+}
+
+void GBuffer::CreateDescriptors()
+{
+    // Create SRV to resource so we can sample the gbuffer texture in a shader program.
+    D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+    ZeroMemory(&srvDesc, sizeof(D3D12_SHADER_RESOURCE_VIEW_DESC));
+    srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+    srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+
+    for (UINT i = 0; i < static_cast<UINT>(EGBufferLayer::MAX); i++)
+    {
+        bool isDsv = m_buffer[i].m_isDSV;
+
+        srvDesc.Format = isDsv ? DXGI_FORMAT_R24_UNORM_X8_TYPELESS : m_bufferFormats[i];
+        srvDesc.Texture2D.MipLevels = 1u;
+
+        m_device->CreateShaderResourceView(m_buffer[i].m_resource.Get() , &srvDesc, m_buffer[i].m_hCpuSrv);
+
+        // Create DSV if
+        if (isDsv)
+        {
+            D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+            dsvDesc.Format = m_bufferFormats[i];
+            dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+            dsvDesc.Texture2D.MipSlice = 0u;
+
+            m_device->CreateDepthStencilView(m_buffer[i].m_resource.Get(), &dsvDesc, m_buffer[i].m_hCpuRtvDsv);
+        }
+        // Otherwise – rtv
+        else
+        {
+            D3D12_RENDER_TARGET_VIEW_DESC rtvDesc = {};
+            ZeroMemory(&rtvDesc, sizeof(rtvDesc));
+            rtvDesc.Format = m_bufferFormats[i];
+            rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+            rtvDesc.Texture2D.MipSlice = 0u;
+            rtvDesc.Texture2D.PlaneSlice = 0u;
+
+            m_device->CreateRenderTargetView(m_buffer[i].m_resource.Get(), &rtvDesc, m_buffer[i].m_hCpuRtvDsv);
+        }
+    }
+}
+
 void GBuffer::CreateResources()
 {
+	CD3DX12_RESOURCE_DESC texDesc = {};
+    ZeroMemory(&texDesc, sizeof(CD3DX12_RESOURCE_DESC));
+    texDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+    texDesc.Alignment = (UINT64)0;
+    texDesc.Width = (UINT64)m_width;
+    texDesc.Height = m_height;
+    texDesc.DepthOrArraySize = (UINT16)1;
+    texDesc.MipLevels = (UINT16)0;
+    
+    texDesc.SampleDesc.Count = 1u;
+    texDesc.SampleDesc.Quality = 0u;
+    
+    texDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+    
+    D3D12_CLEAR_VALUE optClear = {};
+    ZeroMemory(&optClear, sizeof(D3D12_CLEAR_VALUE));
+    
+    for (UINT i = 0; i < static_cast<UINT>(EGBufferLayer::MAX); i++)
+    {
+        bool isDsv = m_buffer[i].m_isDSV;
 
+        texDesc.Format = m_bufferFormats[i];
+        texDesc.Flags = isDsv ? D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL : D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
+        
+        optClear.Format = m_bufferFormats[i];
+        if (isDsv)
+        {
+            optClear.DepthStencil.Depth = 1.0f;
+            optClear.DepthStencil.Stencil = (UINT8)0;
+        }
+    
+        ThrowIfFailed(m_device->CreateCommittedResource(
+            &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+            D3D12_HEAP_FLAG_NONE,
+            &texDesc,
+            D3D12_RESOURCE_STATE_COMMON,
+            &optClear,
+            IID_PPV_ARGS(&m_buffer[i].m_resource)));
+    }
 }
--- Engine/Src/Core/GBuffer.h
+++ Engine/Src/Core/GBuffer.h
@@ -1,26 +1,67 @@
 #pragma once
 
-#ifndef GBufferSize
-#define GBufferSize 5 // DiffuseAlbedo, LightAccumulation, Normal, Depth, Specular
-#endif
-
 #include "Common/DXHelper.h"
+
+enum EGBufferLayer : uint8_t
+{
+	DIFFUSE_ALBEDO = 0,
+	LIGHT_ACCUM,
+	NORMAL,
+	SPECULAR,
+	DEPTH,	
+
+	MAX = 5
+};
+
+struct FGBufferTexture
+{
+	ComPtr<ID3D12Resource> m_resource = nullptr;
+	D3D12_CPU_DESCRIPTOR_HANDLE m_hCpuRtvDsv = {};
+	D3D12_CPU_DESCRIPTOR_HANDLE m_hCpuSrv = {};
+
+	bool m_isDSV = false;
+};
 
 class GBuffer final
 {
 public:
-	GBuffer(ID3D12Device* device);
+	GBuffer(ID3D12Device* device, UINT width, UINT height);
 	GBuffer(const GBuffer& buffer) = delete;
 	GBuffer& operator=(const GBuffer& buffer) = delete;
 
 	~GBuffer() noexcept;
 
+public:
+	// if screen resized
+	void OnResize(UINT newWidth, UINT newHeight);
+
+	FGBufferTexture* GetBufferTexture(EGBufferLayer bufferLayer)
+	{
+		return &m_buffer[bufferLayer];
+	}
+
+	DXGI_FORMAT GetBufferTextureFormat(EGBufferLayer bufferLayer)
+	{
+		return m_bufferFormats[bufferLayer];
+	}
+
 private:
 	void CreateResources();
-
+	void CreateDescriptors();
 
 private:
 	ID3D12Device* m_device = nullptr;
-	ComPtr<ID3D12Resource> m_buffer[GBufferSize];
-	DXGI_FORMAT m_bufferFormats[GBufferSize];
+
+	UINT m_width, m_height;
+
+	FGBufferTexture m_buffer[EGBufferLayer::MAX];
+	// maybe better decision to create a map [EGBufferLayer, DXGI_FORMAT]
+	static constexpr DXGI_FORMAT m_bufferFormats[static_cast<int>(EGBufferLayer::MAX)] = // order of DXGI_FORMAT should corresponds to EGBufferLayer 
+	{
+		DXGI_FORMAT_R8G8B8A8_UNORM,			//DIFFUSE_ALBEDO
+		DXGI_FORMAT_R8G8B8A8_UNORM,			//LIGHT_ACCUM
+		DXGI_FORMAT_R32G32B32A32_FLOAT,		//NORMAL
+		DXGI_FORMAT_R8G8B8A8_UNORM,			//SPECULAR
+		DXGI_FORMAT_D24_UNORM_S8_UINT		//DEPTH
+	};
 };
