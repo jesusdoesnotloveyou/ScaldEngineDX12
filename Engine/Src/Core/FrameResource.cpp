b7683e1836318c12369f5785cd2bc9441a4f3381
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-11-18 19:26:07 +0300

    give a name to allocator of frame resource

--- Engine/Src/Core/FrameResource.cpp
+++ Engine/Src/Core/FrameResource.cpp
@@ -4,6 +4,9 @@
 FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount, UINT pointLightsCount)
 {
 	ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(commandAllocator.GetAddressOf())));
+	
+	std::wstring name = L"FrameRes_CmdAllocator_" + std::to_wstring(commandAllocatorIndex++);
+	commandAllocator->SetName(name.c_str());
 
 	ObjectsCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, TRUE);
 	PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, TRUE);
