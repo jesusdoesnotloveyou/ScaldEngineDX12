c02ef5d32d9493b3b274e893faae42ffe7e4c8fe
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-12-25 14:07:22 +0300

    some nmaps added (issue)

--- Engine/Src/Core/Engine.h
+++ Engine/Src/Core/Engine.h
@@ -246,7 +246,7 @@ private:
 
 #pragma region TexturesAndSky
     UINT m_skyCubeSrvHeapStartIndex = 0u;
-    UINT m_diffuseSrvHeapStartIndex = 0u;
+    UINT m_texturesSrvHeapStartIndex = 0u;
     UINT m_normalSrvHeapStartIndex = 0u;
 #pragma endregion TexturesAndSky
 
