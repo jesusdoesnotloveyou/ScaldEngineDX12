2644d64c0e03a7bf1586988d6656b2e62b5a4ed0
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2026-02-07 14:47:23 +0300

    static samplers setting in rs (default/explicit)

--- Engine/Src/Core/RootSignature.h
+++ Engine/Src/Core/RootSignature.h
@@ -13,8 +13,26 @@ public:
 	RootSignature& operator=(RootSignature&& rhs) = delete;*/
 
 public:
+	template<typename T = CD3DX12_STATIC_SAMPLER_DESC>
+	void Create(ID3D12Device* device, UINT numParameters, const CD3DX12_ROOT_PARAMETER* rootParams, D3D12_ROOT_SIGNATURE_FLAGS flags, UINT numStaticSamplers = 0u, const T* staticSamplers = nullptr)
+	{
+		if (numStaticSamplers)
+		{
+			// Root signature is an array of root parameters
+			m_rootSignatureDesc.Init(numParameters, rootParams, numStaticSamplers, staticSamplers, flags);
+		}
+		else
+		{
+			auto staticSamplers = GetStaticSamplers();
+			// Root signature is an array of root parameters
+			m_rootSignatureDesc.Init(numParameters, rootParams, (UINT)staticSamplers.size(), staticSamplers.data(), flags);
+		}
 
-	void Create(ID3D12Device* device, UINT numParameters, const CD3DX12_ROOT_PARAMETER* rootParams, D3D12_ROOT_SIGNATURE_FLAGS flags);
+		ComPtr<ID3DBlob> signature = nullptr;
+		ComPtr<ID3DBlob> error = nullptr;
+		ThrowIfFailed(D3D12SerializeRootSignature(&m_rootSignatureDesc, m_rootSignatureVersion, &signature, &error));
+		ThrowIfFailed(device->CreateRootSignature(0u, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_rootSignature)));
+	}
 
 	FORCEINLINE ID3D12RootSignature* Get() const { return m_rootSignature.Get(); }
 
