dd09614e849012aa96e99e54d6417b22070f138d
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2026-02-18 14:05:36 +0300

    fix weird issue with var name

--- Engine/Src/Core/Engine.h
+++ Engine/Src/Core/Engine.h
@@ -211,7 +211,7 @@ private:
 private:
     std::vector<std::unique_ptr<FrameResource>> m_frameResources;
     FrameResource* m_currFrameResource = nullptr;
-    int m_ñurrFrameResourceIndex = 0;
+    int m_currentFrameResourceIndex = 0;
 
     UINT m_passCbvOffset = 0u;
 
